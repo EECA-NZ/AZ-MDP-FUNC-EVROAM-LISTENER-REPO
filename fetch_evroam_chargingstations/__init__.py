1db7a3052a8aca7616a84ee5428b88ec295b896d
willcattoneeca <91039935+willcattoneeca@users.noreply.github.com>
2024-03-14 15:49:39 +1300

    Willdev retire old import (#9)
    * EVRoam listener, fetch_evroam_sites and fetch_evroam_chargingstations writing to SQL only (no blob output).
    
    * Retire EVRoam aggregator.
    
    * Better transaction management.

--- fetch_evroam_chargingstations/__init__.py
+++ fetch_evroam_chargingstations/__init__.py
@@ -1,4 +1,13 @@
-import azure.functions as func
+"""
+This is a timer-triggered function.
+
+It fetches the latest EV charger charging stations and availability data from the EVRoam
+API and enters it into the SQL database.
+
+It acts as a fallback for the EVRoam webhook listener, in case
+the listener fails to receive the data.
+"""
+
 import os
 import json
 import logging
@@ -6,106 +15,116 @@ import datetime
 import http.client
 import urllib.parse
 import pandas as pd
-from azure.storage.blob import BlobServiceClient
 from inflection import camelize
-
-
-#### Constants based on provided environment
-
+import azure.functions as func
 from constants import *
 from sharedCode import database_utils
-CONNECT_STR = os.getenv('StorageAccountConnectionString')
-CONTAINER_NAME = CSV_FILE_PATH['container']
-CHARGINGSTATIONS_BLOB_NAME = CSV_FILE_PATH['path']['chargingstations']
-AVAILABILITIES_BLOB_NAME = CSV_FILE_PATH['path']['availabilities']
-SUBSCRIPTION_KEY = os.getenv('EvroamSubscriptionKey')
 
-if not CONNECT_STR:
-    raise ValueError("StorageAccountConnectionString is not set or is empty!")
+# Ensure the subscription key is available
+SUBSCRIPTION_KEY = os.getenv("EvroamSubscriptionKey")
 if not SUBSCRIPTION_KEY:
+    logging.error("EvroamSubscriptionKey is not set or is empty!")
     raise ValueError("EvroamSubscriptionKey is not set or is empty!")
 
 
-#### Functions
-
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+    """
+    Main function for the Azure timer trigger that
+    fetches EVRoam charging stations data and writes it to the database.
+    """
+    utc_timestamp = (
+        datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    )
     if mytimer.past_due:
-        logging.info('The timer is past due!')
-    all_data = fetch_evroam_chargingstations_data()
-    if all_data:
-        availabilities, chargingstations = process_data_to_dataframes(all_data)
-        database_utils.write_availabilities_to_db(availabilities)
-        database_utils.write_chargingstations_to_db(chargingstations)
-        logging.info("Charging Station and Availability data written to SQL Database")
-        upload_csv_to_blob(availabilities, AVAILABILITIES_BLOB_NAME)
-        upload_csv_to_blob(chargingstations, CHARGINGSTATIONS_BLOB_NAME)
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+        logging.warning("The timer is past due!")
+
+    try:
+        all_data = fetch_evroam_chargingstations_data()
+        if all_data:
+            availabilities, chargingstations = process_data_to_dataframes(all_data)
+            logging.info("Charging Station and Availability data processed")
+            database_utils.write_availabilities_to_db(availabilities)
+            database_utils.write_chargingstations_to_db(chargingstations)
+            logging.info(
+                "Charging Station and Availability data written to SQL Database"
+            )
+        else:
+            logging.warning("No data was fetched from EVRoam.")
+    except Exception as exc: # pylint: disable=broad-except
+        logging.error(
+            "Failed to fetch and process EVRoam charging stations data: %s", exc
+        )
+
+    logging.info("Python timer trigger function ran at %s", utc_timestamp)
 
 
 def fetch_evroam_chargingstations_data():
-    headers = {
-        'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY,
-    }
+    """
+    Fetches EVRoam charging stations data from the API.
+    """
+    headers = {"Ocp-Apim-Subscription-Key": SUBSCRIPTION_KEY}
     all_data = []
     result_page = 1
     while True:
-        params = urllib.parse.urlencode({
-            'resultPage': result_page,
-        })
+        params = urllib.parse.urlencode({"resultPage": result_page})
         try:
-            conn = http.client.HTTPSConnection('evroam.azure-api.net')
-            conn.request("GET", "/consumer/api/ChargingStation?%s" % params, "{body}", headers)
+            conn = http.client.HTTPSConnection("evroam.azure-api.net")
+            conn.request(
+                "GET", f"/consumer/api/ChargingStation?{params}", "{body}", headers
+            )
             response = conn.getresponse()
-            data = json.loads(response.read())
-            all_data.extend(data['chargingStations'])
-            conn.close()
-            if not data["hasMoreResults"]:
+            if response.status in (200, 202):
+                data = json.loads(response.read())
+                all_data.extend(data["chargingStations"])
+                logging.info(
+                    "Successfully fetched page %s: %s charging stations",
+                    result_page,
+                    len(data["chargingStations"]),
+                )
+                if not data["hasMoreResults"]:
+                    break
+                result_page += 1
+            else:
+                logging.error(
+                    "Failed to fetch data: HTTP %s - %s",
+                    response.status,
+                    response.reason,
+                )
                 break
-            result_page += 1
-        except Exception as e:
-            print(e)
+        except http.client.HTTPException as http_exc:
+            logging.error("Error fetching data from EVRoam: %s", http_exc)
             break
+        finally:
+            conn.close()
     return all_data
 
 
 def process_data_to_dataframes(all_data):
-    df = pd.json_normalize(all_data)
-    for c in CHARACTERS_TO_REPLACE:
-        df.columns = [
-            col.replace(c, ' ')
-            for col in df.columns
-        ]
-    df.columns = [
-        camelize(col, True).replace(' ', '')
-        for col in df.columns
-    ]
+    """
+    Processes raw EVRoam charging stations data into pandas DataFrames for
+    charging stations and availabilities.
+    """
+    dataframe = pd.json_normalize(all_data)
+    corrected_columns = []
+    for col in dataframe.columns:
+        for character in CHARACTERS_TO_REPLACE:
+            col = col.replace(character, " ")
+        col = camelize(col.strip(), uppercase_first_letter=True).replace(" ", "")
+        corrected_columns.append(col)
+    dataframe.columns = corrected_columns
 
-    availabilities = df[AVAILABILITIES_COLUMNS]
-    availabilities = availabilities.drop_duplicates(
-        subset=JSON_KEYS['availabilities']
-    )
-    availabilities.dropna(
-        inplace=True,
-        subset=JSON_KEYS['availabilities'],
-        how='any'
+    availabilities_df = dataframe[AVAILABILITIES_COLUMNS].copy()
+    availabilities_df.drop_duplicates(inplace=True, subset=JSON_KEYS["availabilities"])
+    availabilities_df.dropna(
+        inplace=True, subset=JSON_KEYS["availabilities"], how="any"
     )
 
-    chargingstations = df.drop(columns=CHARGINGSTATIONS_DROP_COLUMNS)
-    chargingstations = chargingstations.drop_duplicates(
-        subset=JSON_KEYS['chargingstations']
+    chargingstations_df = dataframe.drop(columns=CHARGINGSTATIONS_DROP_COLUMNS).copy()
+    chargingstations_df.drop_duplicates(
+        inplace=True, subset=JSON_KEYS["chargingstations"]
     )
-    chargingstations.dropna(
-        inplace=True,
-        subset=JSON_KEYS['chargingstations'],
-        how='any'
+    chargingstations_df.dropna(
+        inplace=True, subset=JSON_KEYS["chargingstations"], how="any"
     )
-    return availabilities, chargingstations
 
-
-def upload_csv_to_blob(df, blob_name):
-    csv_str = df.to_csv(index=False)
-    blob_service_client = BlobServiceClient.from_connection_string(CONNECT_STR)
-    blob_client = blob_service_client.get_blob_client(CONTAINER_NAME, blob_name)
-    blob_client.upload_blob(csv_str, overwrite=True)
+    return availabilities_df, chargingstations_df
--- fetch_evroam_sites/__init__.py
+++ fetch_evroam_sites/__init__.py
@@ -1,4 +1,13 @@
-import azure.functions as func
+"""
+This is a timer-triggered function. 
+
+It fetches the latest EV charger site data from the EVRoam
+API and enters it into the SQL database.
+
+It acts as a fallback for the EVRoam webhook listener, in case
+the listener fails to receive the data.
+"""
+
 import os
 import json
 import logging
@@ -6,79 +15,95 @@ import datetime
 import http.client
 import urllib.parse
 import pandas as pd
-from azure.storage.blob import BlobServiceClient
 from inflection import camelize
-
-
-#### Constants based on provided environment
-
+import azure.functions as func
 from constants import *
 from sharedCode import database_utils
 
-CONNECT_STR = os.getenv('StorageAccountConnectionString')
-CONTAINER_NAME = CSV_FILE_PATH['container']
-BLOB_NAME = CSV_FILE_PATH['path']['sites']
-SUBSCRIPTION_KEY = os.getenv('EvroamSubscriptionKey')
-
-if not CONNECT_STR:
-    raise ValueError("StorageAccountConnectionString is not set or is empty!")
+# Ensure the subscription key is available
+SUBSCRIPTION_KEY = os.getenv("EvroamSubscriptionKey")
 if not SUBSCRIPTION_KEY:
+    logging.error("EvroamSubscriptionKey is not set or is empty!")
     raise ValueError("EvroamSubscriptionKey is not set or is empty!")
 
 
-#### Functions
-
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+    """
+    Main function for the Azure timer trigger that
+    fetches EVRoam sites data and writes it to the database.
+    """
+    utc_timestamp = (
+        datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    )
     if mytimer.past_due:
-        logging.info('The timer is past due!')
-    all_data = fetch_evroam_sites_data()
-    if all_data:
-        df = process_data_to_dataframe(all_data)
-        logging.info(f"Collected site data: {len(df)} rows")
-        database_utils.write_sites_to_db(df)
-        logging.info("Site data written to SQL Database")
-        upload_csv_to_blob(df)
-        logging.info(f"Delivered site data to {BLOB_NAME}")
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+        logging.warning("The timer is past due!")
+
+    try:
+        all_data = fetch_evroam_sites_data()
+        if all_data:
+            data_frame = process_data_to_dataframe(all_data)
+            logging.info("Collected site data: %s rows", len(data_frame))
+            database_utils.write_sites_to_db(data_frame)
+            logging.info("Site data successfully written to SQL Database")
+        else:
+            logging.warning("No data was fetched from EVRoam.")
+    except Exception as exc:  # pylint: disable=broad-except
+        logging.error("Failed to fetch and process EVRoam sites data: %s", exc)
+
+    logging.info("Python timer trigger function ran at %s", utc_timestamp)
 
 
 def fetch_evroam_sites_data():
-    headers = {
-        'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY,
-    }
+    """
+    Fetches EVRoam sites data from the API.
+    """
+    headers = {"Ocp-Apim-Subscription-Key": SUBSCRIPTION_KEY}
     all_data = []
     result_page = 1
     while True:
-        params = urllib.parse.urlencode({
-            'resultPage': result_page,
-        })
+        params = urllib.parse.urlencode({"resultPage": result_page})
         try:
-            conn = http.client.HTTPSConnection('evroam.azure-api.net')
-            conn.request("GET", "/consumer/api/Site?%s" % params, "{body}", headers)
+            conn = http.client.HTTPSConnection("evroam.azure-api.net")
+            conn.request("GET", f"/consumer/api/Site?{params}", "{body}", headers)
             response = conn.getresponse()
-            data = json.loads(response.read())
-            all_data.extend(data["sites"])
-            conn.close()
-            if not data["hasMoreResults"]:
+            if response.status in (200, 202):
+                data = json.loads(response.read())
+                all_data.extend(data["sites"])
+                logging.info(
+                    "Successfully fetched page %s: %s sites",
+                    result_page,
+                    len(data["sites"]),
+                )
+                if not data["hasMoreResults"]:
+                    break
+                result_page += 1
+            else:
+                logging.error(
+                    "Failed to fetch data: HTTP %s - %s",
+                    response.status,
+                    response.reason,
+                )
                 break
-            result_page += 1
-        except Exception as e:
-            print(e)
+        except http.client.HTTPException as http_exc:
+            logging.error("Error fetching data from EVRoam: %s", http_exc)
             break
+        finally:
+            conn.close()
     return all_data
 
 
 def process_data_to_dataframe(all_data):
-    df = pd.DataFrame(all_data)
-    df.columns = [camelize(col, True) for col in df.columns]
-    df.drop_duplicates(inplace=True, subset=JSON_KEYS['sites'])
-    return df
-
-
-def upload_csv_to_blob(df):
-    csv_str = df.to_csv(index=False)
-    blob_service_client = BlobServiceClient.from_connection_string(CONNECT_STR)
-    blob_client = blob_service_client.get_blob_client(CONTAINER_NAME, BLOB_NAME)
-    blob_client.upload_blob(csv_str, overwrite=True)
+    """
+    Processes raw EVRoam sites data into a pandas DataFrame.
+    """
+    data_frame = pd.DataFrame(all_data)
+    corrected_columns = []
+    for col in data_frame.columns:
+        for character in CHARACTERS_TO_REPLACE:
+            col = col.replace(character, " ")
+        col = camelize(col.strip(), uppercase_first_letter=True).replace(" ", "")
+        corrected_columns.append(col)
+    data_frame.columns = corrected_columns
+    data_frame.drop_duplicates(inplace=True, subset=JSON_KEYS["sites"])
+    logging.info("DataFrame prepared with %s unique sites.", len(data_frame))
+    return data_frame
